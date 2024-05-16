e61f84caeb3e8373f8bf3539511ec02e00142a17
Austin Grantham <agrantham@algidious.com>
2024-05-15 18:23:05 -0700

    reverting hardcoded db values

--- PTWebSocket/Fishing/DBConn.js
+++ PTWebSocket/Fishing/DBConn.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql2');
 const dbconn = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'ronaVirus22!',
-    database: 'FishyRich',
-    port: '3306',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     connectionLimit: 200
 });
 
