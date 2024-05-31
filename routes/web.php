52c7a88bc5ae4b3c2a9d234532fdcc3384f40158
Ubuntu <ubuntu@ip-172-31-91-198.ec2.internal>
2024-05-31 01:29:10 +0000

    adding fishforfun.net config

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;								 
 // Route::namespace('Frontend')->middleware(['siteisclosed', 'checker'])->group(function () {
-// Route::domain('dtmay60dlp20i.cloudfront.net/')->
-Route::namespace('Frontend')->middleware(['siteisclosed','checker'])->group(function () {
+Route::domain('games'.config('app.domain'))->namespace('Frontend')->middleware(['siteisclosed','checker'])->group(function () {
 
 
     Route::get('login', [
