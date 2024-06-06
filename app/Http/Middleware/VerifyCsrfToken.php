e87ece15baa599440fbc10f4d11d0b90c766a0e7
Austin <austingranthamdev90@gmail.com>
2024-06-06 10:08:01 -0700

    disable csrf token for all routes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -4,13 +4,14 @@ namespace VanguardLTE\Http\Middleware
     class VerifyCsrfToken extends \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken
     {
         protected $except = [
-            '/game/*/server', 
-            '/payment/interkassa/result', 
-            '/payment/coinbase/result', 
-            '/payment/btcpayserver/result', 
-            '/sms/callback', 
-            '/profile/contact',
-            '/verifyRGToken'
+            // '/game/*/server', 
+            // '/payment/interkassa/result', 
+            // '/payment/coinbase/result', 
+            // '/payment/btcpayserver/result', 
+            // '/sms/callback', 
+            // '/profile/contact',
+            // '/verifyRGToken',
+            '*',
         ];
     }
 
