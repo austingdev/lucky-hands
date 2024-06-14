2352472be0ffad6d245bf16751c5fa6c809f1c37
Austin <austingranthamdev90@gmail.com>
2024-06-14 00:00:44 -0700

    refetch token

--- resources/js/windows/RelaxGamingLogin.jsx
+++ resources/js/windows/RelaxGamingLogin.jsx
@@ -1,49 +1,61 @@
-import React, { useEffect } from 'react';
-import { EE } from '../App';
-import '../css/login.css';
-import {PAGE_SIZE_DEFAULT} from "../common/Config";
-import { post, relaxGamingVerifyToken } from '../server/server';
+import React, { useCallback, useEffect, useState } from "react";
+import { EE } from "../App";
+import "../css/login.css";
+import { PAGE_SIZE_DEFAULT } from "../common/Config";
+import { post, relaxGamingVerifyToken } from "../server/server";
 
 export const RelaxGamingLogin = () => {
+    const [searchParams, setSearchParams] = useState({});
 
-	useEffect(() => {
-		EE.addListener("RESIZE", onResize);
-		EE.emit("FORCE_RESIZE");
+    useEffect(() => {
+        EE.addListener("RESIZE", onResize);
+        EE.emit("FORCE_RESIZE");
+    }, []);
 
-		verifyToken();
-	}, [])
+    useEffect(() => {
+        const searchParams = new URLSearchParams(window.location.search);
+        setSearchParams({
+            gameId: searchParams.get("gameId"),
+            token: searchParams.get("token"),
+            env: searchParams.get("env"),
+        });
+    }, [window.location.search]);
 
-	const onResize = (data) => {
-		var div = $('#gameLoadingBack');
-		var width = div.width();
-		div.css('height', width * 61 / 673);
-	}
+    useEffect(() => {
+        searchParams?.token && verifyToken();
+    }, [searchParams]);
 
-	const searchParams = React.useMemo(() => new URLSearchParams(window.location.search), [
-    window.location.search
-  ]);
-	const gameId = searchParams.get('gameId')
+    const onResize = (data) => {
+        var div = $("#gameLoadingBack");
+        var width = div.width();
+        div.css("height", (width * 61) / 673);
+    };
 
-	const verifyToken = () => {
-		const token = searchParams.get('token')
-		const env = searchParams.get('env')
-		var _token = document.getElementById('root').getAttribute('token');
+    const verifyToken = useCallback(() => {
+        const { gameId, token, env } = searchParams;
+        var _token = document.getElementById("root").getAttribute("token");
 
-		var data = {gameId, token, env, _token}
-		relaxGamingVerifyToken(data, (res) => {
-			console.log("===Relax Gaming Verify Token Response===", res)
-			window.location.href = '/game/' + gameId + '?api_exit=/';
-		});
-	}
+        var data = { gameId, token, env, _token };
+        relaxGamingVerifyToken(data, (res) => {
+            console.log("===Relax Gaming Verify Token Response===", res);
+            // window.location.href = "/game/" + gameId + "?api_exit=/";
+        });
+    }, [searchParams]);
 
-	return (
-		<div class="popup" id="loading_popup">
-			<div id="loading_popup_image" style={{backgroundImage: `url('/images/splash-screen--${gameId}.jpg')` , backgroundColor: '#cccccc'}}>
-				<div id="gameLoadingBack">
-					<div id="gameLoadingProgress"></div>
-					<p id="progressPercent">0.00%</p>
-				</div>
-			</div>
-		</div>
-	)
-}
+    return (
+        <div class="popup" id="loading_popup">
+            <div
+                id="loading_popup_image"
+                style={{
+                    backgroundImage: `url('/images/splash-screen--${searchParams.gameId}.jpg')`,
+                    backgroundColor: "#cccccc",
+                }}
+            >
+                <div id="gameLoadingBack">
+                    <div id="gameLoadingProgress"></div>
+                    <p id="progressPercent">0.00%</p>
+                </div>
+            </div>
+        </div>
+    );
+};
